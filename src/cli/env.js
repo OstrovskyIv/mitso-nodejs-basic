7cc04bcb744899bdc80308aea6843502eff4b81d
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-15 19:42:09 +0300

    Command line interface (src/cli) – +1 балл - env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = process.env;
+    const result = [];
+
+    for (const key in envVars) {
+        if (key.startsWith('MITSO_')) {
+            result.push(`${key}=${envVars[key]}`);
+        }
+    }
+
+    console.log('\n' + result.join('; ') + '\n');
 };
 
 parseEnv();
+//MITSO_VAR1=hello MITSO_VAR2=world node src/cli/env.js

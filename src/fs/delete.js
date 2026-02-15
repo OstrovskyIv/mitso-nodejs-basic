29125128606a4e189778d1107b36ae73b9e9cad1
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-15 19:27:02 +0300

    File system (src/fs) – +1 балл - delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await fs.rm(filePath);
+    } catch {
+        throw new Error('Ну нет файла тут');
+    }
 };
 
 await remove();

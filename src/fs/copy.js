2de737f94d2cf0ed6c3c2bd7f363476d97ba29d9
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-15 19:26:30 +0300

    File system (src/fs) – +1 балл - copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const srcPath = path.join(__dirname, 'files');
+    const destPath = path.join(__dirname, 'files_copy');
+
+    try {
+        await fs.cp(srcPath, destPath, { recursive: true, errorOnExist: true, force: false });
+    } catch {
+        throw new Error('Нит не работает');
+    }
 };
 
 await copy();

2d1ee466345d3927baac4e018582611524ac08ac
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-15 19:26:46 +0300

    File system (src/fs) – +1 балл - rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const oldPath = path.join(__dirname, 'files', 'wrongFilename.txt');
+    const newPath = path.join(__dirname, 'files', 'properFilename.md');
+
+    try {
+        await fs.access(newPath).then(() => { throw new Error(); }).catch(async (e) => {
+            if (e.message === '') throw e;
+            await fs.rename(oldPath, newPath);
+        });
+    } catch {
+        throw new Error('ОШИИБКА');
+    }
 };
 
 await rename();

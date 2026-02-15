f55b012b13a95979508953d38ce2cbfe30a34d5b
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-15 19:27:17 +0300

    File system (src/fs) – +1 балл - list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const list = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const folderPath = path.join(__dirname, 'files');
+
+    try {
+        const files = await fs.readdir(folderPath);
+        console.log(files);
+    } catch {
+        throw new Error('Папки нет не пиши мне');
+    }
 };
 
 await list();

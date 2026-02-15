e7945e1171802b1005f51a059274800c71042266
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-15 19:26:15 +0300

    File system (src/fs) – +1 балл - create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const create = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await fs.writeFile(filePath, 'АЛООООООЕ ВЕРАА', { flag: 'wx' });
+    } catch {
+        throw new Error('Файл есть');
+    }
 };
 
 await create();

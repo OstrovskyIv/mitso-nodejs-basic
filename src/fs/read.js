a2a5bcdf1951bfa8fba26e972fcff805aa0549d3
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-15 19:27:32 +0300

    File system (src/fs) – +1 балл - read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const content = await fs.readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch {
+        throw new Error('Бабайка');
+    }
 };
 
 await read();

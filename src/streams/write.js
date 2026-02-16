1973d13e407db5480d8adef065383ad334f58364
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-16 09:47:10 +0300

    Streams (src/streams) – +1 балл - write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    const writableStream = fs.createWriteStream(filePath);
+
+    process.stdin.pipe(writableStream);
+
+    console.log('НАЖМИ НА ЧË НИТЬ (для выхода нажми Ctrl+C):');
 };
 
 await write();

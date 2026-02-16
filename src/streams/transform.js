7b4ceebeb6346a6907d34ce17e1eb88d5a0bdb50
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-16 09:47:29 +0300

    Streams (src/streams) – +1 балл - transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const reversed = chunk.toString().trim().split('').reverse().join('');
+            callback(null, reversed + '\n');
+        }
+    });
+
+    console.log('Напиши чё нить и я переверну это (Ctrl+C для выхода):');
+
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();

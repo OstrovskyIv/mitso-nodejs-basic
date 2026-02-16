dc39dc92a0859f1f00f8bd2c6fda2cc20e3a30a5
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-16 09:46:23 +0300

    Modules (src/modules) – +1 балл - calcHash.js + забыл фигню для модуля

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createHash } from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+        const fileBuffer = await fs.readFile(filePath);
+        const hash = createHash('sha256').update(fileBuffer).digest('hex');
+
+        console.log('\nHash:', hash, '\n');
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await calculateHash();

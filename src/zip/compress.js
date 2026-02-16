acc95cdd8230665e326130a0b0917bd2464daaf7
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-16 09:47:57 +0300

    Zlib (src/zip) – +1 балл - compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const sourcePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+    const destinationPath = path.join(__dirname, 'files', 'archive.gz');
+
+    try {
+        await pipeline(
+            fs.createReadStream(sourcePath),
+            zlib.createGzip(),
+            fs.createWriteStream(destinationPath)
+        );
+        console.log('\nФайл успешно сжат в archive.gz\n');
+    } catch {
+        throw new Error('окак');
+    }
 };
 
 await compress();

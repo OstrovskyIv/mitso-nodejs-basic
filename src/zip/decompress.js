51744289ce79db38c67b5b7e19ce7051c85a86dd
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-16 09:48:30 +0300

    Zlib (src/zip) – +1 балл - decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { pipeline } from 'node:stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const sourcePath = path.join(__dirname, 'files', 'archive.gz');
+    const destinationPath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+    try {
+        await pipeline(
+            fs.createReadStream(sourcePath),
+            zlib.createGunzip(),
+            fs.createWriteStream(destinationPath)
+        );
+        console.log('\nФайл успешно распакован\n');
+    } catch {
+        throw new Error('я уже заколебался это делать памагитииии');
+    }
 };
 
 await decompress();

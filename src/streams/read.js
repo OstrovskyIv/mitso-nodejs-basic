3d2efeaf81976b032e9db41ab55866ec116fa371
OstrovskyIv <ostrovskyiml@gmail.com>
2026-02-16 09:46:47 +0300

    Streams (src/streams) – +1 балл - read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const readableStream = fs.createReadStream(filePath, 'utf-8');
+
+    readableStream.pipe(process.stdout);
+
+    readableStream.on('end', () => console.log('\n'));
 };
 
 await read();
